74a4983f8924ba5a54a85bb9e898d498476389fb
delarocamcas <mdelaroca@calacademy.org>
2024-01-24 16:28:26 -0800

    sql get function for collections db
    modified folder structure to be less confusing
    
    removed unnecessary files

--- collections_api/models.py
+++ collections_api/models.py
@@ -1,12 +1,6 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Permission, Group
 
 class Images(models.Model):
     original_filename = models.CharField(max_length=2000, blank=True, null=True)
@@ -23,3 +17,22 @@ class Images(models.Model):
     class Meta:
         managed = False
         db_table = 'images'
+
+# class UserProfile(AbstractUser):
+#     password = models.CharField(max_length=200, blank=True, null=True)
+#
+#     class Meta:
+#         using = "default"
+#         managed = True
+#         db_table = 'auth_user'
+
+
+# class Role(models.Model):
+#     name = models.CharField(max_length=255)
+#     permissions = models.ManyToManyField(Permission, related_name='roles')
+#     class Meta:
+#         managed = True
+#         db_table = 'auth_user'
+#         using = 'default'
+
+
