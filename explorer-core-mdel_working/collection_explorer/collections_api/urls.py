70ac69d0402b59589b5b2c1f3c4eb1a7dece3fc2
chriaa <christinaaasante@gmail.com>
2024-02-16 17:07:20 -0800

    gbif dataset and publications request updates ; and url changes

--- explorer-core-mdel_working/collection_explorer/collections_api/urls.py
+++ explorer-core-mdel_working/collection_explorer/collections_api/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('get_image', views.get_image),
     path('list_images', views.list_images),
     path('get_collections_data', views.get_collections_data),
-    path('get_pubs/<pub_key>/<dataset_key>', views.get_publications),
-    path('get_dataset/<id>/', views.get_datasets)
+    path('get_pubs', views.get_publications),
+    path('get_datasets', views.get_datasets)
     # path('register/', views.create_user),
     # path('login/', views.user_authentication)
 
