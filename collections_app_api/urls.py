c9998ff21e95352a66315cb3c1abf8802077eaa2
jezhuuz <jennazhu@bu.edu>
2024-04-22 14:06:44 -0700

    committing explorer-core

---
+++ collections_app_api/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path, include
+from django.conf import settings
+from . import views
+from .views import *
+from rest_framework import routers
+
+
+# collectionobj_router = routers.SimpleRouter()
+# collectionobj_router.register(
+#     r'collections',
+#     CollectionObjsViewSet,
+#     basename='collections'
+# )
+
+
+urlpatterns = [
+    path('get_image', views.get_image),
+    path('list_images', views.list_images),
+    path('get_collections_data', views.get_collections_data),
+    path('get_publications', views.get_publications, name='get_publications'),
+    path('get_datasets', views.get_datasets, name='get_datasets'),
+    # path('register/', views.create_user),
+    # path('login/', views.user_authentication)
+
+    ##added by jz
+    path('hello/', views.hello_CAS, name='hello_CAS'),
+    path('coordinates/', views.db_coordinates, name="db_coordinates"),
+    path('collection_objects/', views.db_collectionobjects, name="collections_graph_year"),
+    path('collection/', views.db_collection, name="collection_time"),
+    path('collection_map/', views.db_collectiongeo, name='collections_map'),
+    path('collection_images/', views.db_collectionimages, name='collection_images')
+    #path('api/', include(collectionobj_router.urls))
+]
--- explorer-core-mdel_working/collection_explorer/manage.py
+++ explorer-core-mdel_working/collection_explorer/manage.py
@@ -5,10 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    ##original line:
-    ##os.environ.setdefault("DJANGO_SETTINGS_MODULE", "explorer-core.settings")
-    #changed to explorer-core.setting_test for botanydb - jz
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "explorer-core.settings_test")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collection_explorer.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
